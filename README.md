2e2cd596d994ab89e2f653db357d4fe119ac4d55
Efe <52710548+developefe@users.noreply.github.com>
2023-09-13 19:55:34 +0300

    uploaded-project-first-time-to-github

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# crypto-api-react-redux
-A simple app that lists current crypto information via API.
+
+
+# Current Crypto - React/Redux
---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+	Navbar,
+	HomePage,
+	Cryptocurrencies,
+	CryptoDetails,
+	News,
+} from "./components";
+
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="app">
+			<div className="navbar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<BrowserRouter>
+							<Routes>
+								<Route exact path="/" element={<HomePage />} />
+								<Route
+									path="/cryptocurrencies"
+									element={<Cryptocurrencies />}
+								/>
+								<Route path="/crypto/:coindId" element={<CryptoDetails />} />
+								<Route path="/news" element={<News />} />
+							</Routes>
+						</BrowserRouter>
+					</div>
+				</Layout>
+				<div className="footer">
+					<Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+						Crypto <br />
+						<a href="https://github.com/developefe" target="_blank">@developefe</a>
+					</Typography.Title>
+					<Space>
+						<a href="/">Homepage</a>
+						<a href="/news">News</a>
+					</Space>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
